2b4e1398adcb6d544b13cb8ef99964cf9e079e29
Victor Campos <victorboaventcampos@gmail.com>
2022-09-18 20:46:29 -0300

    Treating many openai calls error

--- Codigo/getTopicsAndDescriptionsByRepository.py
+++ Codigo/getTopicsAndDescriptionsByRepository.py
@@ -1,7 +1,5 @@
 import mysql.connector
 import requests
-import openai
-
 from pathlib import Path
 from dotenv import dotenv_values
 
@@ -12,8 +10,6 @@ USER = config["USER"]
 PASSWORD = config["PASSWORD"]
 DATABASE = config["DATABASE"]
 GITHUB_TOKEN = config["GITHUB_TOKEN"]
-
-openai.api_key = config["OPENAI_API_KEY"]
 
 mydb = mysql.connector.connect(
     host=HOST,
@@ -57,81 +53,55 @@ def run_gquery(owner, repository):
 
 mycursor = mydb.cursor()
 
-limit = 100
+limit = 5
 cont = True
 
 while cont:
     mycursor.execute(
         f"SELECT * FROM repository WHERE stars IS NULL LIMIT {limit}")
-    print("Consulta")
+    print(f"Consulta")
 
     myresult = mycursor.fetchall()
-    # print(myresult)
-    # if len(myresult) > 0:
-    for x in myresult:
-        print(x[1])
-        repsplit = x[1].split("/")
-        json = run_gquery(repsplit[0], repsplit[1])
-        if json['data']['repository'] is not None:
-            description = json['data']['repository']['description']
-            print("Descricao: ")
-            print(description)
-            if description is not None:
-                # descriptionSplit = description.split()
-                response = openai.Completion.create(
-                    model="text-davinci-002",
-                    prompt=f"Describe in one word the domain of this GitHub description:{description}",
-                    temperature=0.6,  # aleatoriedade
-                )
-                result = response.choices[0].text.strip()
-                print("Resultado openIA")
-                print(result)
-                if len(result) < 50:
-                    # descriptionSplitNew = [
-                    #     str for str in descriptionSplit if len(str) > 1]
+    if len(myresult) > 0:
+        for x in myresult:
+            print(x[1])
+            repsplit = x[1].split("/")
+            json = run_gquery(repsplit[0], repsplit[1])
+            if json['data']['repository'] is not None:
+                description = json['data']['repository']['description']
+                if description is not None:
+                    descriptionSplit = description.split()
+                    descriptionSplitNew = [
+                        str for str in descriptionSplit if len(str) > 1]
                     sql = "INSERT INTO repository_description (repository_id, word) VALUES (%s, %s)"
                     ll = []
-                    # for d in descriptionSplitNew:
-                    ll.append((x[0], result))
-                    try:
-                        mycursor.executemany(sql, ll)
-                        mydb.commit()
-                    except:
-                        print(
-                            "Erro ao executar essa query  (repository_description): ")
-                        print(sql)
-            else:
-                description = ""
-            topics = json['data']['repository']['repositoryTopics']['edges']
-            if len(topics) > 0:
-                print("Salvando topicos")
-                topicsSave = []
-                for t in topics:
-                    topicName = t['node']['topic']['name']
-                    topicsSave.append((x[0], topicName))
-                print(topicsSave)
-                try:
+                    for d in descriptionSplitNew:
+                        if len(d) <= 50:
+                            ll.append((x[0], d))
+                    mycursor.executemany(sql, ll)
+                    mydb.commit()
+
+                topics = json['data']['repository']['repositoryTopics']['edges']
+                if len(topics) > 0:
+                    print("Salvando topicos")
+                    topicsSave = []
+                    for t in topics:
+                        topicName = t['node']['topic']['name']
+                        if len(topicName) <= 30:
+                            topicsSave.append((x[0], topicName))
+                    print(topicsSave)
                     sql = "INSERT INTO repository_tag (repository_id, tag) VALUES (%s, %s)"
                     mycursor.executemany(sql, topicsSave)
                     mydb.commit()
-                except:
-                    print("Erro ao executar essa query (repository_tag): ")
-                    print(sql)
 
-            stars = json['data']['repository']['stargazerCount']
-            # try:
-            #     sql = f"UPDATE repository SET stars = {stars}, full_description = '{description}' WHERE id={x[0]}"
-            #     mycursor.execute(sql)
-            #     mydb.commit()
-            # except:
-            #     print("Erro ao executar essa query  (repository): ")
-            #     print(sql)
-
-        else:
-            sql = f"UPDATE repository SET stars = 0 WHERE id={x[0]}"
-            mycursor.execute(sql)
-            mydb.commit()
-        print("---")
+                stars = json['data']['repository']['stargazerCount']
+                sql = f"UPDATE repository SET stars = {stars} WHERE id={x[0]}"
+                mycursor.execute(sql)
+                mydb.commit()
+            else:
+                sql = f"UPDATE repository SET stars = 0 WHERE id={x[0]}"
+                mycursor.execute(sql)
+                mydb.commit()
     else:
         cont = False
 
--- Codigo/openIA.py
+++ Codigo/openIA.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import openai
 import re
-
+import time
 from pathlib import Path
 from dotenv import dotenv_values
 
@@ -11,7 +11,7 @@ HOST = config["HOST"]
 USER = config["USER"]
 PASSWORD = config["PASSWORD"]
 DATABASE = config["DATABASE"]
-OPENAI_API_KEY = config["OPENAI_API_KEY"]
+openai.api_key = config["OPENAI_API_KEY"]
 
 mydb = mysql.connector.connect(
     host=HOST,
@@ -21,7 +21,39 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-openai.api_key = OPENAI_API_KEY
+
+def db_execute(sql):
+    try:
+        mycursor.execute(sql)
+        mydb.commit()
+    except mysql.connector.Error as err:
+        print(f"Error at query: {sql}")
+        print("Something went wrong: {}".format(err))
+
+
+def db_execute_many(sql, arrayOfRecords):
+    try:
+        mycursor.executemany(sql, arrayOfRecords)
+        mydb.commit()
+    except mysql.connector.Error as err:
+        print(f"Error at query: {sql}")
+        print("Something went wrong: {}".format(err))
+
+
+def execute_openia(task):
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=task,
+            temperature=0.6,  # randomness
+        )
+        return response.choices[0].text.strip().replace(
+            '\n', ' ').replace('\r', '').replace('  ',  ' ')
+    except:
+        print(f"Something went wrong at exeute_openia: {task}")
+        print("Waiting one minute")
+        time.sleep(60)
+
 
 limit = 100
 
@@ -32,60 +64,52 @@ print("Fetching database repositories and descriptions...")
 print("---\n")
 
 repositories = mycursor.fetchall()
-temperature = 0.6
+# temperature = 0.6
 
 if len(repositories) > 0:
     for repository in repositories:
         print("---")
-        
+
         repository_id = repository[0]
         repository_name_with_owner = repository[1]
-        repository_description = repository[5].decode() # decode() to remove b'' of MySQL TEXT type 
+        # decode() to remove b'' of MySQL TEXT type
+        repository_description = repository[5].decode()
         description = repository_description
 
         print("Repository: " + repository_name_with_owner)
         print("Description: " + description)
 
-        if description is not None and len(description)>0:
+        if description is not None and len(description) > 0:
             first_question_for_openia = "Describe the domain of this software from this description"
-            response = openai.Completion.create(
-                model="text-davinci-002",
-                prompt=f"{first_question_for_openia}:{description}",
-                temperature=temperature, # randomness
-            )
-            domain_description_by_openia = response.choices[0].text.strip()
+            domain_description_by_openia = execute_openia(
+                f"{first_question_for_openia}:{description}")
+            if domain_description_by_openia is None:
+                continue
             print("OpenIA domain description: " + domain_description_by_openia)
-            try:
-                sql = f"INSERT INTO repository_description_openia (repository_id, description) VALUES ({repository_id}, '{domain_description_by_openia}')"
-                mycursor.execute(sql)
-                mydb.commit()
-            except:
-                print(
-                    "Error executing this query (repository_description_openia): ")
-                print(sql)
-            
+            sql = f"INSERT INTO repository_description_openia (repository_id, description) VALUES ({repository_id}, '{domain_description_by_openia}')"
+            # time.sleep(10)
+            db_execute(sql)
+
             second_question_for_openia = "Extract a comma-separated list of categories in one word each category from the software description"
-            responseDaResponse = openai.Completion.create(
-                model="text-davinci-002",
-                prompt=f"{second_question_for_openia}:{description}",
-                temperature=temperature,  # randomness
-            )
-            categories_by_openia = responseDaResponse.choices[0].text.strip().replace('\n', ' ').replace('\r', '').replace('  ',  ' ')
+            categories_by_openia = execute_openia(
+                f"{second_question_for_openia}:{description}")
+            if categories_by_openia is None:
+                continue
             categories_by_openia = categories_by_openia.split(',')
             print("OpenIA domain categories: ")
             print(categories_by_openia)
-            for category in categories_by_openia:
-                category = re.sub('[^A-Za-z0-9- ]+', '', category)
-                category = category.lstrip()
-                if(category is not None and len(category)>0):
-                    try:
-                        sql = f"INSERT INTO repository_category_openia (repository_id, category) VALUES ({repository_id}, '{category}')"
-                        mycursor.execute(sql)
-                        mydb.commit()
-                    except:
-                        print(
-                            "Error executing this query (repository_category_openia): ")
-                        print(sql)
+            if len(categories_by_openia) > 0:
+                categoryToSave = []
+                for category in categories_by_openia:
+                    category = re.sub('[^A-Za-z0-9- ]+', '', category)
+                    category = category.lstrip()
+                    if(category is not None and len(category) > 0 and category != ''):
+                        categoryToSave.append((repository_id, category))
+
+                if(len(categoryToSave) > 0):
+                    sql = "INSERT INTO repository_category_openia (repository_id, category) VALUES (%s, %s)"
+                    # time.sleep(10)
+                    db_execute_many(sql, categoryToSave)
 
         print("---\n")
 else:
