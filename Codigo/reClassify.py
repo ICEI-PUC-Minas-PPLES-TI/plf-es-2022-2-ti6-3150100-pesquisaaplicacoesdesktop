fa99a1dd3218f77226229ec27a7b7969fdcd281b
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-10-12 11:13:37 -0300

    add pagiation to reClassify script, now classify all repositories

--- Codigo/reClassify.py
+++ Codigo/reClassify.py
@@ -57,56 +57,65 @@ def execute_openia(task):
         print("Waiting one minute")
         time.sleep(60)
 
+def populate_description(limit, offset):
+    mycursor.execute(
+        f"SELECT * FROM repository LIMIT {limit} OFFSET {offset}")
+    print("---\n")
+    print("Fetching database repositories and descriptions...")
+    print("---\n")
+
+    repositories = mycursor.fetchall()
+    # temperature = 0.6
+
+    if len(repositories) > 0:
+        for repository in repositories:
+            print("---")
+
+            repository_id = repository[0]
+            repository_name_with_owner = repository[1]
+            # decode() to remove b'' of MySQL TEXT type
+            repository_description = repository[5].decode()
+            description = repository_description
+
+            print("Repository: " + repository_name_with_owner)
+            print("Description: " + description)
+
+            if description is not None and len(description) > 0:
+                question_for_openia = f"Classify the repository description {description} according to the following categories: [{categoriesString}]"
+                rulesForQuestion =  "If it suits to more than one category, write then comma separated. If it doens't fit any category, write None."
+                categories_choosen_by_openia = execute_openia(
+                    f"{question_for_openia}.{rulesForQuestion}")
+                if categories_choosen_by_openia is None:
+                    continue
+                
+                categories_by_openia = categories_choosen_by_openia.split(',')
+                print("OpenIA classified word(s): ")
+                print(categories_by_openia)
+                
+                if len(categories_by_openia) > 0:
+                    categoryToSave = []
+                    for category in categories_by_openia:
+                        category = re.sub('[^A-Za-z0-9- ]+', '', category)
+                        category = category.lstrip()
+                        if(category is not None and len(category) > 0 and category != ''):
+                            categoryToSave.append((repository_id, category))
+
+                    if(len(categoryToSave) > 0):
+                        sql = "INSERT INTO repository_description (repository_id, word) VALUES (%s, %s)"
+                        # time.sleep(10)
+                        db_execute_many(sql, categoryToSave)
+
+            print("---\n")
+
+        offset = offset + limit
+        populate_description(limit, offset)
+
+    elif offset == 0:
+        print("No repositories found!")
+    else:
+        print("--Finished--")
+
 
 limit = 100
-
-mycursor.execute(
-    f"SELECT * FROM repository LIMIT {limit}")
-print("---\n")
-print("Fetching database repositories and descriptions...")
-print("---\n")
-
-repositories = mycursor.fetchall()
-# temperature = 0.6
-
-if len(repositories) > 0:
-    for repository in repositories:
-        print("---")
-
-        repository_id = repository[0]
-        repository_name_with_owner = repository[1]
-        # decode() to remove b'' of MySQL TEXT type
-        repository_description = repository[5].decode()
-        description = repository_description
-
-        print("Repository: " + repository_name_with_owner)
-        print("Description: " + description)
-
-        if description is not None and len(description) > 0:
-            question_for_openia = f"Classify the repository description {description} according to the following categories: [{categoriesString}]"
-            rulesForQuestion =  "If it suits to more than one category, write then comma separated. If it doens't fit any category, write None."
-            categories_choosen_by_openia = execute_openia(
-                f"{question_for_openia}.{rulesForQuestion}")
-            if categories_choosen_by_openia is None:
-                continue
-            
-            categories_by_openia = categories_choosen_by_openia.split(',')
-            print("OpenIA classified word(s): ")
-            print(categories_by_openia)
-            
-            if len(categories_by_openia) > 0:
-                categoryToSave = []
-                for category in categories_by_openia:
-                    category = re.sub('[^A-Za-z0-9- ]+', '', category)
-                    category = category.lstrip()
-                    if(category is not None and len(category) > 0 and category != ''):
-                        categoryToSave.append((repository_id, category))
-
-                if(len(categoryToSave) > 0):
-                    sql = "INSERT INTO repository_description (repository_id, word) VALUES (%s, %s)"
-                    # time.sleep(10)
-                    db_execute_many(sql, categoryToSave)
-
-        print("---\n")
-else:
-    print("No repositories without OpenIA data found!")
+offset = 0
+populate_description(limit, offset)
